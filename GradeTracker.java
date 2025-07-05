843b37adb3f528b6996b397e032fcd16fe69115c
jazil <jaazilmohamed@gmail.com>
2025-07-05 10:10:52 +0530

    Add "Display Summary"

--- GradeTracker.java
+++ GradeTracker.java
@@ -5,14 +5,23 @@ public class GradeTracker{
 
   public GradeTracker() {
     this.students = new ArrayList<>();
-}
+  }
 
-//Add a new student
-public void addStudent(Student student){
+  //Add a new student
+  public void addStudent(Student student){
   students.add(student);
-}
+  }
 
-//Display summary
-public void displaySummary(){
-  System.out.println("\n@@@ ")
+  //Display summary
+  public void displaySummary(){
+    System.out.println("\n@@@ Student Grade Summary @@@");
+    for (Student student : students){
+    System.out.println("Name: " + student.getName());
+    System.out.println("Grades: " + student.getGrades());
+    System.out.println("Average: " + student.getAverage());
+    System.out.println("Highest: " + student.getHighest());
+    System.out.println("Lowest: " + student.getLowest());
+    System.out.println("@@@------------------------@@@");
+    }
+  }
 }
